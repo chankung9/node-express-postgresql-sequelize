2002b5ab7e490b21c25a4a6be223b4b38fdd48c5
worrapong limsupreeyalat <chankung9@gmail.com>
2021-04-03 11:08:53 +0700

    edit port

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 const creds = {
   development: {
-    username: "root",
-    password: "password",
-    // database: process.env.DB_NAME,
-    host: "localhost",
-    dialect: 'mysql'
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USERNAME,
